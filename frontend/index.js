80ecd016b689e51c13c9ce2effdab5801a96c70b
Megan Freitas <meganfreitas74@icloud.com>
2024-06-25 23:14:56 -0500

    need to fix the url code

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,8 @@
 async function moduleProject4() {
-
   // 👇 WORK WORK BELOW THIS LINE 👇
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+  const footer = document.querySelector('footer');
+  const currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
 
   let descriptions = [
     ["Sunny", "☀️"],
@@ -12,12 +11,33 @@ async function moduleProject4() {
     ["Thunderstorm", "⛈️"],
     ["Snowy", "❄️"],
     ["Partly Cloudy", "⛅️"]
-  ]
+  ];
 
   // 👉 Tasks 1 - 5 go here
+  document.querySelector('#weatherWidget').style.display = 'none';
+  const citySelect = document.querySelector('#citySelect');
+  citySelect.addEventListener('change', async function(event) {
+    console.log('selection changed');
+    try {
+      citySelect.setAttribute('disabled', 'disabled');
+      document.querySelector('#weatherWidget').style.display = 'none';
+      document.querySelector('.info').textContent = 'Fetching weather data...';
+    console.log(event.target.value);
+    let city = event.target.value;
+    let url = `http://localhost:3003/api/weather?city=${city}`;
+   
+    const res = await axios.get(url);
+
+    document.querySelector('#weatherWidget').style.display = 'block';
+    document.querySelector('.info').textContent = '';
+    EventTarget.target.removeAttribute('disabled');
+    console.log(res.data);
+  } catch (err) { 
+      console.log('promise rejected with an err.message ==>', err.message);
+    }
+  });
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
-
 }
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
